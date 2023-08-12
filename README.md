ecb3aaffb650833e1643e5b3e102cc2518615f2a
dhslrl321 <dhslrl321@gmail.com>
2023-08-12 18:03:30 +0900

    commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Kafka
 
+### run
+
+runner 에서 kafka-ui 가 포함된 kafka 클러스터를 실행시킬 수 있습니다
---
+++ kafka-producer/src/test/java/com/github/dhslrl321/Main.java
@@ -0,0 +1,7 @@
+package com.github.dhslrl321;
+
+import org.springframework.kafka.test.context.EmbeddedKafka;
+
+@EmbeddedKafka
+public class Main {
+}
